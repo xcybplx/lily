9cdd9c8069bfff63134c4084cd5e6d9390077e88
xcybplx <piotr.niemidomski@gmail.com>
2022-06-16 11:20:46 +0200

    Cleanup
    Cleanup

--- esphome/components/network/__init__.py
+++
@@ -1,7 +0,0 @@
-import esphome.codegen as cg
-
-CODEOWNERS = ["@esphome/core"]
-AUTO_LOAD = ["mdns"]
-
-network_ns = cg.esphome_ns.namespace("network")
-IPAddress = network_ns.class_("IPAddress")
--- esphome/components/nfc/__init__.py
+++
@@ -1,7 +0,0 @@
-import esphome.codegen as cg
-
-CODEOWNERS = ["@jesserockz"]
-
-nfc_ns = cg.esphome_ns.namespace("nfc")
-
-NfcTag = nfc_ns.class_("NfcTag")
